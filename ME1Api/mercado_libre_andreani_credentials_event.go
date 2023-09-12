625734960626022284716fc706be47e94b0f3334
architecture-it <ciberseguridad@andreani.com>
2023-09-12 17:37:08 +0000

    add ME1Api schemas

--- ME1Api/mercado_libre_andreani_credentials_event.go
+++ ME1Api/mercado_libre_andreani_credentials_event.go
@@ -25,7 +25,7 @@ type MercadoLibreAndreaniCredentialsEvent struct {
 	UserMeliId int32 `json:"UserMeliId"`
 }
 
-const MercadoLibreAndreaniCredentialsEventAvroCRC64Fingerprint = "\xef-s.q$\xe4\xc1"
+const MercadoLibreAndreaniCredentialsEventAvroCRC64Fingerprint = "\x8a\xa9j\xeaxx\x9e\x83"
 
 func NewMercadoLibreAndreaniCredentialsEvent() MercadoLibreAndreaniCredentialsEvent {
 	r := MercadoLibreAndreaniCredentialsEvent{}
@@ -77,11 +77,11 @@ func (r MercadoLibreAndreaniCredentialsEvent) Serialize(w io.Writer) error {
 }
 
 func (r MercadoLibreAndreaniCredentialsEvent) Schema() string {
-	return "{\"fields\":[{\"name\":\"ClientId\",\"type\":\"string\"},{\"name\":\"ContractId\",\"type\":\"string\"},{\"name\":\"UserMeliId\",\"type\":\"int\"}],\"name\":\"Andreani.Me1Api.Events.Record.MercadoLibreAndreaniCredentialsEvent\",\"type\":\"record\"}"
+	return "{\"fields\":[{\"name\":\"ClientId\",\"type\":\"string\"},{\"name\":\"ContractId\",\"type\":\"string\"},{\"name\":\"UserMeliId\",\"type\":\"int\"}],\"name\":\"Andreani.Me1Api.Events.MercadoLibreAndreaniCredentialsEvent\",\"type\":\"record\"}"
 }
 
 func (r MercadoLibreAndreaniCredentialsEvent) SchemaName() string {
-	return "Andreani.Me1Api.Events.Record.MercadoLibreAndreaniCredentialsEvent"
+	return "Andreani.Me1Api.Events.MercadoLibreAndreaniCredentialsEvent"
 }
 
 func (_ MercadoLibreAndreaniCredentialsEvent) SetBoolean(v bool)    { panic("Unsupported operation") }
